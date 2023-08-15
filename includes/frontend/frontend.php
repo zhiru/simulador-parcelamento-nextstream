d404051e3432cf0562b07d8743fda211656589e5
Felipe <felipe@aireset.com.br>
2023-08-15 15:42:38 +0000

    update

--- includes/frontend/frontend.php
+++ includes/frontend/frontend.php
@@ -1,6 +1,7 @@
 <?php
 function simulador_parcelamento_nextstream_enqueue_scripts() {
-    wp_enqueue_script('simulador-parcelamento-scripts', plugins_url('assets/js/simulador.js', SPNEXT_DIR), array('jquery'), null, true);
+    //wp_enqueue_script('simulador-parcelamento-scripts', plugins_url('assets/js/simulador.js', SPNEXT_DIR), array('jquery'), null, true);
+    wp_enqueue_script(SPNEXT_SLUG . '-frontend-js', SPNEXT_URL . 'assets/js/simulador.js', array('jquery'), SPNEXT_VERSION, true);
 
     // Localize o script para passar dados para o JavaScript
     wp_localize_script('simulador-parcelamento-scripts', 'simulador_params', array(
@@ -12,7 +13,8 @@ add_action('wp_enqueue_scripts', 'simulador_parcelamento_nextstream_enqueue_scri
 
 // frontend.php
 function simulador_parcelamento_nextstream_enqueue_styles() {
-    wp_enqueue_style( 'simulador-parcelamento-styles', plugins_url( 'assets/css/style.css', SPNEXT_DIR ) );
+    // wp_enqueue_style( 'simulador-parcelamento-styles', plugins_url( 'assets/css/style.css', SPNEXT_DIR ) );    
+    wp_enqueue_style(SPNEXT_SLUG . '-frontend-css', SPNEXT_URL . 'assets/css/style.css', array(), SPNEXT_VERSION, 'all');
 }
 add_action( 'wp_enqueue_scripts', 'simulador_parcelamento_nextstream_enqueue_styles' );
 
--- simulador-parcelamento-nextstream.php
+++ simulador-parcelamento-nextstream.php
@@ -6,12 +6,21 @@ Version: 1.2.7
 Author: Sylas Filho
 Author URI: https://nextstream.com.br
 */
+if (! defined('ABSPATH'))
+{
+    exit;
+}
 
-define('SPNEXT_DIR', plugin_dir_path(__FILE__));
+
+define('SPNEXT_PATH', plugin_dir_path(__FILE__));
+define('SPNEXT_URL', plugin_dir_url(__FILE__));
+define('SPNEXT_VERSION', '1.2.7');
+define('SPNEXT_NAME', 'Simulador de Parcelamento Nextstream');
+define('SPNEXT_SLUG', 'simulador-parcelamento-nextstream');
 
 // Função para incluir os arquivos necessários
 function simulador_parcelamento_nextstream_include_files() {
-    include_once SPNEXT_DIR . 'includes/admin/admin.php';
-    include_once SPNEXT_DIR . 'includes/frontend/frontend.php';
+    include_once SPNEXT_PATH . 'includes/admin/admin.php';
+    include_once SPNEXT_PATH . 'includes/frontend/frontend.php';
 }
 add_action('plugins_loaded', 'simulador_parcelamento_nextstream_include_files');
